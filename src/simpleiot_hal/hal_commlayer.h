97fa9703c4e1f73d0a9ee95b6255086f7f9ab645
divanchykhin <di@o-log-n.com>
2015-08-14 08:13:13 +0300

    toward firmware for retransmitter

--- src/simpleiot_hal/hal_commlayer.h
+++ src/simpleiot_hal/hal_commlayer.h
@@ -40,7 +40,12 @@ extern "C" {
 
 bool communication_initialize();
 void communication_terminate();
+#ifdef MESH_TEST
+uint8_t hal_send_packet( MEMORY_HANDLE mem_h, uint8_t bus_id, uint8_t intrabus_id );
+uint8_t hal_get_busid_of_last_packet(); // TODO: this approach seems not to be too good interface solution
+#else
 uint8_t send_message( MEMORY_HANDLE mem_h );
+#endif
 uint8_t hal_get_packet_bytes( MEMORY_HANDLE mem_h );
 void keep_transmitter_on( bool keep_on );
 
